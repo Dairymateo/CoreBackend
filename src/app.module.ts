941214bc58356f8f9f4f45095fbcd9923fed0b99
Mateo Herrera <105869739+Dairymateo@users.noreply.github.com>
2025-05-08 19:45:16 -0500

    cambio nuevo

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,15 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
-import { MongooseModule } from '@nestjs/mongoose';
-
-import config from './config/config';
-import { AuthModule } from './auth/auth.module';
-import { PilotsModule } from './pilots/pilots.module';
-import { VehiclesModule } from './vehicles/vehicles.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PilotsModule } from './pilots/pilots.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
+import config from './config/config';
 
 @Module({
   imports: [
@@ -21,27 +20,26 @@ import { AppService } from './app.service';
       isGlobal: true,
       cache: true,
       load: [config],
-      envFilePath: ['.env', '/etc/secrets/.env'],
     }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      inject: [ConfigService],
-      global: true,
-      useFactory: async (cs: ConfigService) => ({
-        secret: cs.get<string>('jwt.secret'),
-      }),
+      useFactory: async (config) => ({
+        secret: config.get('jwt.secret'),
+    }),
+    global: true,
+    inject: [ConfigService],
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (cs: ConfigService) => ({
-        uri: cs.get<string>('database.connectionString'),
+      useFactory: async (config) => ({
+        uri: config.get('database.connectionString'),
       }),
+      inject: [ConfigService],
     }),
     AuthModule,
     PilotsModule,
     VehiclesModule,
-  ],
+   ],
   controllers: [AppController],
   providers: [AppService],
 })
